29578f6ace26690e35716c792fb712c5a091b50f
iliasprc <iliaspapastrat@gmail.com>
2021-02-18 21:24:13 +0200

    init file

--- videoaugment/transforms/__init__.py
+++ videoaugment/transforms/__init__.py
@@ -1,10 +1,11 @@
 from .crop import RandomCrop, RandomResizedCrop, CenterCrop
-from .flip import VerticalFlip, HorizontalFlip,RandomVerticalFlip,RandomHorizontalFlip
-from .general import ComposeSpatialTransforms, ComposeTemporalTransforms, VideoTransform, NumpyToTensor,PILToNumpy,PILToTensor,Normalize,RearrangeTensor
+from .flip import VerticalFlip, HorizontalFlip, RandomVerticalFlip, RandomHorizontalFlip
+from .general import ComposeSpatialTransforms, ComposeTemporalTransforms, VideoTransform, NumpyToTensor, PILToNumpy, \
+    PILToTensor, Normalize, RearrangeTensor,VideoToTensor
 from .geometric import GaussianBlur
 from .intensity import Hue, RandomHue, Brightness, RandomBrightness, Contrast, RandomContrast, Saturation, \
     RandomSaturation, RandomColorAugment
-from .rotate import Rotation, RandomRotation
-from .temporal_transform import TemporalElasticTransformation, TemporalDownsample, TemporalDownsample, Upsample, \
-    RandomTemporalDownsample, TemporalRandomCrop, TemporalCenterCrop
 from .resize import Resize
+from .rotate import Rotation, RandomRotation
+from .temporal_transform import TemporalElasticTransformation, TemporalDownsample, Upsample, \
+    RandomTemporalDownsample, TemporalRandomCrop, TemporalCenterCrop, TemporalScale
